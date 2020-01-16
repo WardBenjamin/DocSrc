ea7a9fdbd4aac73c5b56d551642466a31f5ab3db
Daltz333 <darasmith@wccnet.edu>
2020-01-16 12:59:21 -0500

    implement oh no page

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ author = 'Benjamin Ward, Solomon Greenberg'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_tabs.tabs', 'sphinx_sitemap'
+    'sphinx_tabs.tabs', 'sphinx_sitemap', 'notfound.extension'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
